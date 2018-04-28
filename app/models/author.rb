f87a5d8cb60226000cdba653e5386215ab23b98e
Joao Cito <citojoao@gmail.com>
2018-04-28 16:15:55 -0300

    Devise

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
-class Lawyer < ApplicationRecord
+class Author < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :lawyers
+  devise_for :authors
   resources :suits
   resources :lawyers
-  resources :clients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
